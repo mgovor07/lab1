bd39644f5d0640c6f7e076b9452ccd084c952750
mgovor07 <mgovor07@gmail.com>
2025-09-07 14:16:15 +0300

    Initial Commit

---
+++ govorukhina_lab1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  govorukhina_lab1
+//
+//  Created by Мария Говорухина on 07.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+    
+}
