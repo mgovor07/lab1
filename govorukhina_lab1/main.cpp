c10d8aace08010fb45fb96e48d0a7a9efeb5af17
mgovor07 <mgovor07@gmail.com>
2025-09-07 15:25:27 +0300

    add struct Pipe and CompressorStation

--- govorukhina_lab1/main.cpp
+++ govorukhina_lab1/main.cpp
@@ -6,11 +6,23 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <limits>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
 
-    
-}
+struct Pipe {
+    string name;
+    double length;
+    int diameter;
+    bool underRepair;
+};
+
+struct CompressorStation {
+    string name;
+    int totalWorkshops;
+    int activeWorkshops;
+    int stationClass;
+};
